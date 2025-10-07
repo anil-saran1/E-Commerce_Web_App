f376849c8656a9108532f1bd0fae256e8caa74ed
Anil Saran <saranil1112000@gmail.com>
2025-10-07 20:09:03 +0530

    E-Commerce Web App

---
+++ eommerce-app/src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Hero from '../../components/hero/Hero';
+import LatestCollection from '../../components/latestCollection/LatestCollection';
+import BestSeller from '../../components/bestSeller/BestSeller';
+import OurPolicies from '../../components/ourPolicies/OurPolicies';
+import Subscribe from '../../components/subscribe/Subscribe';
+
+const Home = () => {
+  return (
+    <>
+    <Hero />
+    <LatestCollection />
+    <BestSeller />
+    <OurPolicies />
+    <Subscribe />
+    
+    </>
+  )
+}
+
+export default Home
